44eeac1895b20c955c9afc76a60cd0d8b306150b
solmi <103156290+solmi26@users.noreply.github.com>
2024-06-09 16:56:19 +0900

    [level 0] Title: 조건 문자열, Time: 0.05 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
@@ -1,16 +1,21 @@
 class Solution {
     public int solution(String ineq, String eq, int n, int m) {
-        int answer = Math.max(n, m);
-        int big = m;
-
+        int answer = Math.max(n, m); //실제 큰 수
+        //입력값의 등호가 크다고 가르키는 수를 담기 위한 변수
+        //기본적으로 m으로 할당
+        int input_big = m; 
+        
+     //입력값의 등호가 크다고 가르키는 수를 input_big 변수에 담음
      if (ineq.equals(">")) {
-            big = n;
+            input_big = n;
             if (eq.equals("=")) {
-                big = n;
+                input_big = n;
             }
         }
-
-        if (answer == big) {
+        
+        //실제 큰 수와 입력값의 등호가 크다고 가르키는 수를 비교
+        //실제 큰 수와 같으면 1, 틀리면 0
+        if (answer == input_big) {
             return 1;
         }
         return 0;
