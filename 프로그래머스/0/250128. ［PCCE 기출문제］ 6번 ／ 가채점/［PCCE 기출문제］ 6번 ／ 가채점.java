471b7423a68d7e9d59696cc1213da21fa1513f86
solmi <103156290+solmi26@users.noreply.github.com>
2024-06-12 14:21:46 +0900

    Update ［PCCE 기출문제］ 6번 ／ 가채점.java
    문제 유형 관련 내용 추가

--- 프로그래머스/0/250128. ［PCCE 기출문제］ 6번 ／ 가채점/［PCCE 기출문제］ 6번 ／ 가채점.java
+++ 프로그래머스/0/250128. ［PCCE 기출문제］ 6번 ／ 가채점/［PCCE 기출문제］ 6번 ／ 가채점.java
@@ -1,3 +1,8 @@
+// 디버깅 문제
+// 디버깅(Debugging)은 이미 완성된 코드에서 버그를 찾아 수정하는 문제 타입입니다.
+// 1줄만 수정하여 버그를 고치세요.
+// 2줄 이상 수정할 경우, 실행 결과에 에러 메시지가 표시됩니다.
+
 class Solution {
     public String[] solution(int[] numbers, int[] our_score, int[] score_list) {
         int num_student = numbers.length;
