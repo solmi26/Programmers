93ed9c2160d2760204623026e5a2227675efaf87
solmi <103156290+solmi26@users.noreply.github.com>
2024-06-12 14:18:45 +0900

    Update ［PCCE 기출문제］ 6번 ／ 가채점.java

--- 프로그래머스/0/250128. ［PCCE 기출문제］ 6번 ／ 가채점/［PCCE 기출문제］ 6번 ／ 가채점.java
+++ 프로그래머스/0/250128. ［PCCE 기출문제］ 6번 ／ 가채점/［PCCE 기출문제］ 6번 ／ 가채점.java
@@ -4,7 +4,8 @@ class Solution {
         String[] answer = new String[num_student];
 
         for (int i = 0; i < num_student; i++) {
-            if (our_score[i] == score_list[numbers[i]-1]) {
+         // if (our_score[i] == score_list[i]) { // 기존코드
+            if (our_score[i] == score_list[numbers[i]-1]) { // 수정코드
                 answer[i] = "Same";
             }
             else {
