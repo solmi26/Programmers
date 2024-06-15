d1f01773e29aac2ce5b2ff398046673b5de5579b
solmi <103156290+solmi26@users.noreply.github.com>
2024-06-15 14:55:21 +0900

    Update 문자열 겹쳐쓰기.java
    문제 풀이 추가

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,5 +1,10 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
+        // 첫번째로 생각한 방식
+        /*String answer = my_string.substring(0,s) + overwrite_string + my_string.substring(overwrite_string.length()+s);
+        return answer;*/
+
+        // 두번째로 생각한 방식
         StringBuffer sb = new StringBuffer(my_string);
         sb.replace(s,s+overwrite_string.length(),overwrite_string);
         return sb.toString();
