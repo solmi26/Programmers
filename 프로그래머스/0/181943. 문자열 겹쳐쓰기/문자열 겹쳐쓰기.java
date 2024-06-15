a1fbd6838b45c85c4363be363f0f322a1b04dc52
solmi <103156290+solmi26@users.noreply.github.com>
2024-06-15 14:50:35 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.18 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,6 +1,7 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        String answer = my_string.substring(0,s) + overwrite_string + my_string.substring(overwrite_string.length()+s);
-        return answer;
+        StringBuffer sb = new StringBuffer(my_string);
+        sb.replace(s,s+overwrite_string.length(),overwrite_string);
+        return sb.toString();
     }
 }
