9b86f85d1c7f415e159a98b7c5f5e049b358e17f
solmi <103156290+solmi26@users.noreply.github.com>
2024-06-18 14:24:39 +0900

    Update flag에 따라 다른 값 반환하기.java

--- 프로그래머스/0/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.java
+++ 프로그래머스/0/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.java
@@ -1,6 +1,7 @@
 class Solution {
     public int solution(int a, int b, boolean flag) {        
-        return flag == true ? a+b : a-b;
+        //return flag == true ? a+b : a-b;
+        retrun flag ? a+b : a-b;
         
     }
 }
