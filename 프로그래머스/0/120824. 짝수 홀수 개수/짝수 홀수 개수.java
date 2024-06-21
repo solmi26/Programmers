3f391f8469e574b8ceeaec82654f08716c8ddeed
solmi <103156290+solmi26@users.noreply.github.com>
2024-06-21 15:14:14 +0900

    Update 짝수 홀수 개수.java
    풀이 추가

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -2,11 +2,13 @@ class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[2];
         for(int n:num_list){
-            if(n%2==0){
-                answer[0]++;
-            }else{
-                answer[1]++;
-            }
+            answer[n%2]++; // 홀수면 인덱스 1, 짝수면 인덱스 0을 1씩 증가
+            
+            // if(n%2==0){
+            //     answer[0]++;
+            // }else{
+            //     answer[1]++;
+            // }
         }
         return answer;
     }
