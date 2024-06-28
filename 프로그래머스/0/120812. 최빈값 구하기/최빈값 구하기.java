b3e142f6940d99d588ccbb2f1a94b76adca66c1b
solmi <103156290+solmi26@users.noreply.github.com>
2024-06-28 10:30:45 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.15 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int solution(int[] array) {
+        if(array.length == 1) return array[0];
+        
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int n:array){
+            map.put(n, map.getOrDefault(n, 0)+1);
+        }
+
+        int mode = 0;
+        int value = 0;
+        int maxValue = 0;
+        for(Integer key:map.keySet()){
+            value = map.get(key); 
+            if(maxValue<value){
+                mode = key;
+                maxValue = value;
+            }else if(maxValue==value) {
+                maxValue = -1;}
+        }
+        if(maxValue == -1){
+            return -1;
+        } else{
+            return mode;
+        }
+
+        
+    }
+}
