aa62e15492c61569ef616eafce0e381857b1c3a9
solmi <103156290+solmi26@users.noreply.github.com>
2024-06-29 17:14:23 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.02 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -1,13 +1,10 @@
 class Solution {
     public int[] solution(int[] array) {
         int[] answer = new int[2];
-        answer[0] = array[0];
-        answer[1] = 0;
         
-        for(int i=1; i<array.length; i++){
-            int n = array[i];
-            if(answer[0]<n){
-                answer[0]=n;
+        for(int i=0; i<array.length; i++){
+            if(answer[0]<array[i]){
+                answer[0]=array[i];
                 answer[1]=i;
             }
         }
