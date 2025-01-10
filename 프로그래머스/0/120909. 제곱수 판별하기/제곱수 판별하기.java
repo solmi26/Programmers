8a56cbde63d5b595125d25863e79459c4fa7c1eb
solmi <103156290+solmi26@users.noreply.github.com>
2025-01-10 13:38:56 +0900

    Update 제곱수 판별하기.java

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -9,5 +9,23 @@ class Solution {
             }
         }
         return answer;
+    
+        /**
+        for(int i=1; i*i<=n; i++){ // i*i번째까지만 확인하면 
+            if(i*i == n) {
+                answer = 1;
+                break; // break 문 넣어서 제곱근 찾으면 멈추기
+            }
+        }
+        return answer;
+        **/
+
+        /**
+         if (n % Math.sqrt(n) == 0) { // 내장 함수 이용하기
+            return 1;
+        } else {
+            return 2;
+        }
+        **/
     }
 }
