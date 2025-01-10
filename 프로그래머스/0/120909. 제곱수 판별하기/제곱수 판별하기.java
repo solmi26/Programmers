727dbf90d42a25ea8662af77b286787fdc47fcf9
solmi <103156290+solmi26@users.noreply.github.com>
2025-01-10 13:36:17 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -2,8 +2,11 @@ class Solution {
     public int solution(int n) {
         int answer = 2;
         
-        for(int i=1; i<n; i++){
-            if(i*i == n) answer = 1;
+        for(int i=1; i*i<=n; i++){
+            if(i*i == n) {
+                answer = 1;
+                break;
+            }
         }
         return answer;
     }
