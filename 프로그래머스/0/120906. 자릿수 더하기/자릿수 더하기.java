5cfd7611274e3d55d003f5f292f36857d274f96a
solmi <103156290+solmi26@users.noreply.github.com>
2025-01-17 14:46:34 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        String str = Integer.toString(n);
+        
+        for(int i=0; i<str.length(); i++){
+            answer += str.charAt(i) - '0';
+        }
+        
+        
+        return answer;
+    }
+}
