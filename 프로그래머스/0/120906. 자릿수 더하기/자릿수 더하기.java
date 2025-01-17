7e0220d07b3faf38ebc13fdbb8669ff6bc3230e8
solmi <103156290+solmi26@users.noreply.github.com>
2025-01-17 14:50:43 +0900

    Update 자릿수 더하기.java

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -7,6 +7,13 @@ class Solution {
         for(int i=0; i<str.length(); i++){
             answer += str.charAt(i) - '0';
         }
+
+        /**
+        while(n>0){
+            answer += n%10;
+            n /= 10;
+        }
+        **/
         
         
         return answer;
