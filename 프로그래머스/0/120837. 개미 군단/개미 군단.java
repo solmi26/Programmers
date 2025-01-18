fb1f26d2c1db73f95a3ba6c839f2c2c97b03a83e
solmi <103156290+solmi26@users.noreply.github.com>
2025-01-18 14:32:24 +0900

    Update 개미 군단.java

--- 프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -15,7 +15,20 @@ class Solution {
             }
         }
 
+        /**
+        hp가 5 미만이어도 hp / 5의 결과는 0이고, hp % 5의 결과는 그대로 hp이기 때문에, 굳이 while문으로 조건을 확인할 필요가 없다.
         
-        return answer;
+            answer += hp / 5; // 장군개미
+            hp %= 5;
+    
+            answer += hp / 3; // 병정개미
+            hp %= 3;
+    
+            answer += hp; // 일개미
+            
+            return answer;
+            **/
+            
+            return answer;
     }
 }
