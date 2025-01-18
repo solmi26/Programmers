f47dc94d0fb118f5acbd519d2be4e0ad5c926815
solmi <103156290+solmi26@users.noreply.github.com>
2025-01-18 14:43:41 +0900

    Update 가위 바위 보.java

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -10,6 +10,12 @@ class Solution {
             }else{
                 answer += "2";
             }
+
+            /**
+            if(rsp.charAt(i) == '2') answer += "0";
+            else if(rsp.charAt(i) == '0') answer += "5";
+            else answer += "2";
+            **/
         }
         
         return answer;
