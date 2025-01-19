25be510f1cfc4724efaa12192ee2a4dcbaa1bb03
solmi <103156290+solmi26@users.noreply.github.com>
2025-01-19 15:30:54 +0900

    Update 최댓값 만들기 （2）.java

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -8,6 +8,13 @@ class Solution {
                 if(max > answer) answer = max; 
             }
         }
+
+        /**
+        numbers[0]*numbers[1] vs numbers[numbers.length-1] * numbers[numbers.length-2];
+
+        int min_num = Integer.MIN_VALUE;
+        System.out.println(min_num); // -2147483648(정수형의 최소값)
+        **/
         
         return answer;
     }
