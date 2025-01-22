061cd0fde687ee232afc39c9ffb8f10f4fca0b65
solmi <103156290+solmi26@users.noreply.github.com>
2025-01-22 15:42:24 +0900

    Update 배열 비교하기.java

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -9,5 +9,12 @@ class Solution {
             if(Arrays.stream(arr1).sum() == Arrays.stream(arr2).sum()) return 0;
             else return Arrays.stream(arr1).sum() > Arrays.stream(arr2).sum() ? 1 : -1; 
         }
+
+        /**
+        Integer.compare(x, y) -> x == y라면, 0을 반환.
+                                 x < y라면, -1을 반환.
+                                 x > y라면, 1을 반환.
+        **/
+        
     }
 }
