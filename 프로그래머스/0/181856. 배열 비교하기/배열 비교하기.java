d0a78bbe889707f783cb446332742fe6810af8ce
solmi <103156290+solmi26@users.noreply.github.com>
2025-01-22 15:36:36 +0900

    [level 0] Title: 배열 비교하기, Time: 1.09 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        
+        if(arr1.length > arr2.length) return 1;
+        else if(arr1.length < arr2.length) return -1;
+        else{
+            if(Arrays.stream(arr1).sum() == Arrays.stream(arr2).sum()) return 0;
+            else return Arrays.stream(arr1).sum() > Arrays.stream(arr2).sum() ? 1 : -1; 
+        }
+    }
+}
