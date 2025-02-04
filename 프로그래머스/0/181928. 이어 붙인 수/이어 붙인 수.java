e6fbf3e88a17833520ebf8381a7b475514c7013b
solmi <103156290+solmi26@users.noreply.github.com>
2025-02-04 22:14:44 +0900

    [level 0] Title: 이어 붙인 수, Time: 13.31 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] num_list) {
+        String odd = "";
+        String even = "";
+        
+        for(int num:num_list){
+            if(num%2==0) even += num;
+            else odd += num;
+        }
+        
+        return Integer.parseInt(even) + Integer.parseInt(odd);
+    }
+}
