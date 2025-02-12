414523133854aea5ed5cb3356d89aa7aa3924780
solmi <103156290+solmi26@users.noreply.github.com>
2025-02-12 13:16:51 +0900

    Update n개 간격의 원소들.java

--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+
 class Solution {
     public int[] solution(int[] num_list, int n) {
+
+        ArrayList<Integer> answer = new ArrayList<>();
+        
+        for(int i=0; i<num_list.length; i+=n){
+            answer.add(num_list[i]);
+        }
+        return answer.stream().mapToInt(i->i).toArray();
+
+        /** array로 코딩테스트 문제 풀어보기!
         int[] answer = new int[(int) Math.ceil((double)num_list.length/n)];
         int index = 0;
         
@@ -9,5 +20,6 @@ class Solution {
         }
         
         return answer;
+        **/
     }
 }
