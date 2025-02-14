240f0361323c302dc5b38eb1fc3510fdbd469d5a
solmi <103156290+solmi26@users.noreply.github.com>
2025-02-14 11:50:56 +0900

    Update 순서 바꾸기.java

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -11,5 +11,15 @@ class Solution {
             answer[idx++] = num_list[i];
         }
         return answer;
+
+
+        /** 
+        // 반복문을 한번만 쓰려고 했는데 유지보수 및 효율상 위에 코드가 더 낫다고 판단하여 수정함.
+         for(int i=0; i<len; i++){
+            if(n == len) n = 0;
+            answer[i] = num_list[n];
+            n++;   
+        }
+        **/
     }
 }
