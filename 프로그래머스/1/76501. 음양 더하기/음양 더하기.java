e64fd9966abe37ec8c9cddf604c22a5c58c1f87d
solmi <103156290+solmi26@users.noreply.github.com>
2025-02-28 14:41:01 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 88.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        
+        for(int i=0; i<signs.length; i++){
+            int num = absolutes[i];
+            if(signs[i]) answer += num;
+            else answer += num * -1;
+        }
+        
+        
+        
+        return answer;
+    }
+}
