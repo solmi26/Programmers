5ff0eff3ff6502bcbfd3cd3b731a66eec8211aca
solmi <103156290+solmi26@users.noreply.github.com>
2025-03-03 15:09:51 +0900

    Update 귤 고르기.java

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -3,11 +3,14 @@ import java.util.*;
 class Solution {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
+        // HashMap = 중복 허용X
         Map<Integer, Integer> countMap = new HashMap<>();
         
         // 1. 귤 크기별 개수 카운팅
         for (int t : tangerine) {
             countMap.put(t, countMap.getOrDefault(t, 0) + 1);
+            // countMap.getOrDefault(t, 0): 기존에 저장된 값이 있으면 가져오고, 없으면 0을 기본값으로 반환.
+            // +1을 해서 해당 귤 크기의 개수를 1 증가시킨다.
         }
         
         // 2. 개수를 내림차순 정렬
