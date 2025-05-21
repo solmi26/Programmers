f1ab11da714e4fdad998386a0f1a75568961b867
solmi <103156290+solmi26@users.noreply.github.com>
2025-05-21 17:55:06 +0900

    [level 0] Title: 모스부호 (1), Time: 0.21 ms, Memory: 91.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class Solution {
+    public String solution(String letter) {
+        
+        Map<String, String> morse = new HashMap<>();
+        
+        morse.put(".-", "a");
+        morse.put("-...", "b");
+        morse.put("-.-.", "c");
+        morse.put("-..", "d");
+        morse.put(".", "e");
+        morse.put("..-.", "f");
+        morse.put("--.", "g");
+        morse.put("....", "h");
+        morse.put("..", "i");
+        morse.put(".---", "j");
+        morse.put("-.-", "k");
+        morse.put(".-..", "l");
+        morse.put("--", "m");
+        morse.put("-.", "n");
+        morse.put("---", "o");
+        morse.put(".--.", "p");
+        morse.put("--.-", "q");
+        morse.put(".-.", "r");
+        morse.put("...", "s");
+        morse.put("-", "t");
+        morse.put("..-", "u");
+        morse.put("...-", "v");
+        morse.put(".--", "w");
+        morse.put("-..-", "x");
+        morse.put("-.--", "y");
+        morse.put("--..", "z");
+        
+        StringBuilder sb = new StringBuilder();
+        String[] str = letter.split(" ");
+        
+        for(String s:str){
+            sb.append(morse.get(s));
+        }
+        
+        
+        return sb.toString();
+    }
+}
